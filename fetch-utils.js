d59fa15f327905d3071e426f606a8b98848f7670
karl wernsman <wernsmank@gmail.com>
2022-09-28 14:27:41 -0700

    Added SUPABASE links to fetch-utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,6 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://cugbcofvobrtranzaycv.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1Z2Jjb2Z2b2JydHJhbnpheWN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQyOTQ3MTAsImV4cCI6MTk3OTg3MDcxMH0.xCDgB_VrvVOssr-xnhoZpEi0uyvXVf8ZXxpCG73jilE';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
